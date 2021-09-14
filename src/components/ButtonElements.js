078ce32dd63c95e4f51fc01a8211a20b1587dd58
Richard-Mbingi <mbingicoding@gmail.com>
2021-09-14 14:28:19 +0300

    Information sections added

--- src/components/ButtonElements.js
+++ src/components/ButtonElements.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { Link } from "react-scroll";
 
+//TODO: Button arrow orientation
 export const Button = styled(Link)`
   border-radius: 50px;
   background: ${({ primary }) => (primary ? "#01BF71" : "#010606")};
---
+++ src/components/InfoSection/data.js
@@ -0,0 +1,55 @@
+//TODO: SVG loading
+
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topline: "Topline",
+  headline: "Sed ut perspiciatis unde omnis iste natus",
+  description:
+    "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni",
+  buttonLabel: "Get started",
+  imgStart: false,
+  img: require("../../images/svg-1.svg"),
+  alt: "Label",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topline: "Topline",
+  headline: "Sed ut perspiciatis unde omnis iste natus",
+  description:
+    "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni",
+  buttonLabel: "Get started",
+  imgStart: true,
+  img: require("../../images/svg-2.svg"),
+  alt: "Label",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: "signup",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topline: "Topline",
+  headline: "Sed ut perspiciatis unde omnis iste natus",
+  description:
+    "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni",
+  buttonLabel: "Start Now",
+  imgStart: false,
+  img: require("../../images/svg-3.svg"),
+  alt: "Paper",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
