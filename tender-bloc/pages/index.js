e8942ae25c5297d4e36b2a8e1404300506092a58
Richard Mbingi <64406225+Richard-Mbingi@users.noreply.github.com>
2021-10-09 19:56:16 +0300

    Form styling

--- tender-bloc/pages/index.js
+++ tender-bloc/pages/index.js
@@ -1,9 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Form from "../components/Form";
+
 
 export default function Home() {
   return (
-    <h1 className='text-xs'>Hello Tailwind</h1>
+    <Form />
   )
 }
