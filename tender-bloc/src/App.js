ff77189ba101e17f122d530f4fc48ad041f5caee
Richard Mbingi <64406225+Richard-Mbingi@users.noreply.github.com>
2021-10-13 21:07:17 +0300

    Tailwind configuration

--- tender-bloc/src/App.js
+++ tender-bloc/src/App.js
@@ -2,8 +2,12 @@
 
 function App() {
   return (
-    <div className="App">
-      <p>Test</p>
+    <div className="self-center">
+      <p class="text-lg font-semibold">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
     </div>
   );
 }
