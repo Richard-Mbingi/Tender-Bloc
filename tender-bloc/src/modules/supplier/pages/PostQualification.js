b775cce5a75cbb99f4730e75d77511a726eba0d7
Richard Mbingi <64406225+Richard-Mbingi@users.noreply.github.com>
2022-01-08 12:27:48 +0300

    Refactor delete

--- tender-bloc/src/modules/supplier/pages/PostQualification.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Page from "../components/Page";
-
-const PostQualification = () => {
-  return <Page name="Post Qualification"></Page>;
-};
-
-export default PostQualification;
