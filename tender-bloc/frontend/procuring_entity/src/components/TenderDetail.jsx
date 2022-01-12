f56ef10e0beb52a870212b46faeeb41a3e901ffc
Richard Mbingi <64406225+Richard-Mbingi@users.noreply.github.com>
2022-01-12 22:22:01 +0300

    Tender details

--- tender-bloc/frontend/procuring_entity/src/components/TenderDetail.jsx
+++ tender-bloc/frontend/procuring_entity/src/components/TenderDetail.jsx
@@ -1,11 +1,76 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { MdDownloadForOffline } from "react-icons/md";
+import { Link, useParams } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
-const TenderDetail = () => {
-     return (
-          <div>
-               TenderDetail
+import { client, urlFor } from "../client";
+import HorizontalLayout from "./HorizontalLayout";
+import { tenderDetailMoreTenderQuery, tenderDetailQuery } from "../utils/data";
+import Spinner from "./Spinner";
+
+const TenderDetail = ({ user }) => {
+  const [tenders, setTenders] = useState(null);
+  const [tenderDetail, setTenderDetail] = useState(null);
+
+  const { tenderId } = useParams();
+
+  const fetchTenderDetails = () => {
+    let query = tenderDetailQuery(tenderId);
+
+    if (query) {
+      client.fetch(query).then((data) => {
+        setTenderDetail(data[0]);
+
+        if (data[0]) {
+          query = tenderDetailMoreTenderQuery(data[0]);
+
+          client.fetch(query).then((res) => setTenders(res));
+        }
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchTenderDetails();
+  }, [tenderId]);
+
+  if (!tenderDetail) return <Spinner message="Loading Tenders" />;
+
+  return (
+    <div
+      className="felx xl:flex-row flex-col m-auto bg-white"
+      style={{ maxWidth: "1500px", borderRadius: "32px" }}
+    >
+      <div className="flex justify-center items-center md:items-start flex-initial">
+        <img
+          src={tenderDetail?.image && urlFor(tenderDetail.image).url()}
+          className="rounded-t-3xl rounde-b-lg"
+          alt="user-post"
+        />
+      </div>
+      <div className="w-full p-5 flex-1 xl:min-w-full">
+        <div className="flex items-center justify-between">
+          <div className="flex gap-2 items-center ">
+            <a
+              href={`${tenderDetail.image?.asset?.url}?dl=`}
+              download
+              onClick={(e) => e.stopPropagation()}
+              className="bg-white w-9 h-9 rounded-full flex items-center justify-center text-black text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
+            >
+              <MdDownloadForOffline />
+            </a>
           </div>
-     )
-}
+          <div>
+               <h1 className="text-4xl font-bold break-words mt-3">
+                    {tenderDetail.title}
+               </h1>
+               <p className="mt-3">{tenderDetail.about}</p>
+          </div>
+          <Link></Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default TenderDetail
+export default TenderDetail;
--- tender-bloc/frontend/procuring_entity/src/utils/data.js
+++ tender-bloc/frontend/procuring_entity/src/utils/data.js
@@ -2,37 +2,37 @@ export const categories = [
   {
     name: "technology",
     image:
-      "https://i.pinimg.com/750x/eb/47/44/eb4744eaa3b3ccd89749fa3470e2b0de.jpg",
+      "https://i.tenderimg.com/750x/eb/47/44/eb4744eaa3b3ccd89749fa3470e2b0de.jpg",
   },
   {
     name: "healthcare",
     image:
-      "https://i.pinimg.com/236x/25/14/29/251429345940a47490cc3d47dfe0a8eb.jpg",
+      "https://i.tenderimg.com/236x/25/14/29/251429345940a47490cc3d47dfe0a8eb.jpg",
   },
   {
     name: "food",
     image:
-      "https://i.pinimg.com/236x/03/48/b6/0348b65919fcbe1e4f559dc4feb0ee13.jpg",
+      "https://i.tenderimg.com/236x/03/48/b6/0348b65919fcbe1e4f559dc4feb0ee13.jpg",
   },
   {
     name: "construction",
     image:
-      "https://i.pinimg.com/750x/66/b1/29/66b1296d36598122e6a4c5452b5a7149.jpg",
+      "https://i.tenderimg.com/750x/66/b1/29/66b1296d36598122e6a4c5452b5a7149.jpg",
   },
   {
     name: "transportation",
     image:
-      "https://i.pinimg.com/236x/72/8c/b4/728cb43f48ca762a75da645c121e5c57.jpg",
+      "https://i.tenderimg.com/236x/72/8c/b4/728cb43f48ca762a75da645c121e5c57.jpg",
   },
   {
     name: "government",
     image:
-      "https://i.pinimg.com/236x/7d/ef/15/7def15ac734837346dac01fad598fc87.jpg",
+      "https://i.tenderimg.com/236x/7d/ef/15/7def15ac734837346dac01fad598fc87.jpg",
   },
   {
     name: "others",
     image:
-      "https://i.pinimg.com/236x/2e/63/c8/2e63c82dfd49aca8dccf9de3f57e8588.jpg",
+      "https://i.tenderimg.com/236x/2e/63/c8/2e63c82dfd49aca8dccf9de3f57e8588.jpg",
   },
 ];
 
@@ -90,3 +90,66 @@ export const feedQuery = `*[_type == 'tender'] | order(_createdAt desc) {
        },
      },
    }`;
+
+   export const tenderDetailQuery = (tenderId) => {
+    const query = `*[_type == "tender" && _id == '${tenderId}']{
+      image{
+        asset->{
+          url
+        }
+      },
+      _id,
+      title, 
+      about,
+      category,
+      destination,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+     save[]{
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      },
+      comments[]{
+        comment,
+        _key,
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      }
+    }`;
+    return query;
+  };
+  
+  export const tenderDetailMoreTenderQuery = (tender) => {
+    const query = `*[_type == "tender" && category == '${tender.category}' && _id != '${tender._id}' ]{
+      image{
+        asset->{
+          url
+        }
+      },
+      _id,
+      destination,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+      save[]{
+        _key,
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      },
+    }`;
+    return query;
+  };
